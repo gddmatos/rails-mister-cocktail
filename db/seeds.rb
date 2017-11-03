fa360d8a618f27b3ad0118ee3d10d84cf54cba38
Guilherme Matos <datwo.matos@gmail.com>
2017-11-03 12:25:00 +0000

    seeding the cocktails

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,16 @@
 require 'json'
 require 'open-uri'
 
-url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
-ingredients_serialized = open(url).read
+ingredient_url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients_serialized = open(ingredient_url).read
 ingredients = JSON.parse(ingredients_serialized)
 ingredients["drinks"].each do |ingredient|
-  Ingredient.create(name: ingredient.values.first)
+ Ingredient.create(name: ingredient["strIngredient1"])
+end
+
+cocktail_url = 'http://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail'
+cocktails_serialized = open(cocktail_url).read
+cocktails = JSON.parse(cocktails_serialized)
+cocktails["drinks"].each do |cocktail|
+  Cocktail.create(name: cocktail["strDrink"])
 end
